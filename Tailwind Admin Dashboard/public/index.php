069b298904fe275d1122b27c74699f5acb5b143c
hridayesh-01 <118613232+hridayesh-01@users.noreply.github.com>
2024-06-22 12:16:31 +0530

    Add files via upload

---
+++ Tailwind Admin Dashboard/public/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tailwindcss Course</title>
+
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/0.0.0-insiders.4a070ac/tailwind.min.css" integrity="sha512-vJu7D5BpjnNXVpLBrl9LKLvmXBNjiLwge8EOZ/YS9XwiChpfKLAlydwIZvoJaDE3LI/kr3goH0MzDzNbBgyoOQ==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+
+    <link rel="stylesheet" href="output.css">
+</head>
+<body>
+    
+    <div class="flex flex-row h-screen">
+        
+
+        <div class="w-64 bg-gray-100 p-4">
+            <div class="flex justify-between items-center flex-row space-x-4">
+				  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-8" viewBox="0 0 20 20" fill="currentColor">
+				  <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+				</svg>
+				<p class=" flex-auto font-regular text-md">John</p>
+                
+                
+            </div>
+        </div>
+
+        <div class="flex-auto bg-blue-200">Main Content</div>
+    </div>
+	
+
+</body>
+</html>
